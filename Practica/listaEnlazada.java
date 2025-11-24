1b214fb56ec57aa6356dd2d9ceab38b38ec49dd8
Nicolas Daniel Roman Moreno <danielrm963@gmail.com>
2025-11-24 11:00:38 -0300

    Lista enlazada

---
+++ Practica/listaEnlazada.java
@@ -0,0 +1,56 @@
+class Node {
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    // Agregar un nodo al final
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    // Imprimir toda la lista
+    public void printList() {
+        Node current = head;//"current" es una variable temp para recorrer la lista
+        while (current != null) {
+            System.out.print(current.value + " --> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class listaEnlazada {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.append(10);
+        list.append(20);
+        //list.append(30);
+
+        list.printList();//"printList" es para imprimir la lista enlazada
+    }
+}
