2aa7f0c2067960dfc5cf1c60b4b38fd51cc8cba0
Nicolas Daniel Roman Moreno <danielrm963@gmail.com>
2025-11-24 21:04:28 -0300

    Modelo de Examen

---
+++ 2do Parcial/Ventas.java
@@ -0,0 +1,12 @@
+public interface Ventas {
+
+  /**
+   * Calcula el precio de venta de la computadora basado en el precio base y el
+   * año actual
+   * 
+   * @param precioBase El precio base de la computadora
+   * @param anioActual El año actual para calcular la depreciación
+   * @return El precio de venta calculado
+   */
+  double calcularPrecioVenta(double precioBase, int anioActual);
+}
